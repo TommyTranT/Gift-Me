33846ac447ce2106c58de28c4e03995ba905bbc5
Tommy Tran <haitran1995@gmail.com>
2022-11-23 02:47:48 -0500

    Add get.query for a specific user

--- server/server.js
+++ server/server.js
@@ -29,7 +29,17 @@ app.post("/users", async (req, res) => {
   }
 });
 
-// get a current user
+// get a specific user
+app.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+
+    res.json(user.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 // WISHLIST
 // create a new wishlist
