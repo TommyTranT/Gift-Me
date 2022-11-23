601d1f7fc23ded388725d593b8a771fc05695688
Tommy Tran <haitran1995@gmail.com>
2022-11-23 02:40:43 -0500

    Add post.query to create new user

--- server/server.js
+++ server/server.js
@@ -14,17 +14,44 @@ app.use(express.json());
 
 // USERS
 // create a new user
+app.post("/users", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *",
+      [username, password]
+    );
+
+    res.json(newUser.rows[0]);
+    console.log("new user registered!");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 // get a current user
 
 // WISHLIST
 // create a new wishlist
 
-// select a wishlist
+// select all wishlist
 
-// edit a wishlist
+// select a specific wishlist
+
+// update a wishlist
 
 // delete a wishlist
+
+// ITEM
+// add a new item
+
+// select all items
+
+// select a specific item
+
+// update an item
+
+// delete an item
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
