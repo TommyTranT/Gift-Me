4d180ff8594a9b657823f2205a2533b9b3f16a72
Tommy Tran <haitran1995@gmail.com>
2022-11-23 02:22:13 -0500

    Add and use cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 8080;
 
 // CONNECT TO DB
@@ -10,6 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 // ROUTES
+
+// USERS
+// create a new user
+
+// get a current user
+
+// WISHLIST
+// create a new wishlist
+
+// select a wishlist
+
+// edit a wishlist
+
+// delete a wishlist
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
