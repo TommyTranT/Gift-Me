a96618a5ba4edb40cc6ec5dfd27044aabd75cdba
Tommy Tran <haitran1995@gmail.com>
2022-12-06 09:19:05 -0500

    materials/ replace form with dialog

--- client/src/components/EditWishlist.jsx
+++ client/src/components/EditWishlist.jsx
@@ -9,9 +9,18 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
+// material ui
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+
 const EditWishlist = ({ wishlist, setWishlists }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [open, setOpen] = useState(false); // state to show dialog form
 
   const updateWishlist = (e) => {
     e.preventDefault();
@@ -24,12 +33,56 @@ const EditWishlist = ({ wishlist, setWishlists }) => {
           setWishlists(res.data);
           setName("");
           setDescription("");
+          setOpen(false); // Close the dialog form
         });
       });
   };
 
+  const handleClickOpen = () => {
+    setOpen(true); // Open the dialog form
+  };
+
+  const handleClickClose = () => {
+    setOpen(false); // Close the dialog form
+  };
+
   return (
     <div>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Edit Wishlist
+      </Button>
+      <Dialog open={open} onClose={handleClickClose}>
+        <DialogTitle>Edit Wishlist</DialogTitle>
+        <DialogContent>
+          <TextField
+            type="text"
+            label={wishlist.name}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+            helperText="Name"
+          ></TextField>
+          <TextField
+            type="text"
+            label={wishlist.description}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+            helperText="Description"
+          ></TextField>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={updateWishlist}>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
       <form onSubmit={(e) => updateWishlist(e)}>
         <input
           type="text"
