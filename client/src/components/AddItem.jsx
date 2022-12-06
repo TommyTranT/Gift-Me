28847ca146f7ce1efcb88bb0ad1f7d946e3d6e04
Tommy Tran <haitran1995@gmail.com>
2022-12-06 09:38:00 -0500

    materials/ replace form with dialog

--- client/src/components/AddItem.jsx
+++ client/src/components/AddItem.jsx
@@ -8,12 +8,21 @@
 import axios from "axios";
 import { useState } from "react";
 
+// material ui
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+
 const AddItem = ({ wishlistID, showWishlists, setShowWishlists }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [img_url, setImgUrl] = useState("");
   const [url, setUrl] = useState("");
+  const [open, setOpen] = useState(false); // state to show dialog form
 
   const onSubmitForm = (e) => {
     e.preventDefault();
@@ -28,45 +37,86 @@ const AddItem = ({ wishlistID, showWishlists, setShowWishlists }) => {
       setUrl("");
 
       setShowWishlists([...showWishlists, res.data]); // State for displaying wishlist plus new data
+      setOpen(false); // Close the dialog form
     });
   };
+
+  const handleClickOpen = () => {
+    setOpen(true); // Open the dialog form
+  };
+
+  const handleClickClose = () => {
+    setOpen(false); // Close the dialog form
+  };
+
   console.log({ wishlistID });
   return (
     <>
-      <form onSubmit={onSubmitForm}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder={"name"}
-        ></input>
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="description"
-        ></input>
-        <input
-          type="text"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          placeholder="price"
-        ></input>
-        <input
-          type="text"
-          value={img_url}
-          onChange={(e) => setImgUrl(e.target.value)}
-          placeholder="imgUrl"
-        ></input>
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          placeholder="url"
-        ></input>
-        <button>Add item</button>
-        <h1>What wishlist is selected: {wishlistID}</h1>
-      </form>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Add Item
+      </Button>
+      <Dialog open={open} onClose={handleClickClose}>
+        <DialogTitle>Add an Item</DialogTitle>
+        <DialogContent>
+          <TextField
+            type="text"
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+          ></TextField>
+          <TextField
+            type="text"
+            label="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+          ></TextField>
+          <TextField
+            type="number"
+            label="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+          ></TextField>
+          <TextField
+            type="text"
+            label="Picture URL"
+            value={img_url}
+            onChange={(e) => setImgUrl(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+          ></TextField>
+          <TextField
+            type="text"
+            label="Direct Link"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            autoComplete="off"
+            margin="dense"
+            variant="standard"
+            fullWidth
+          ></TextField>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={onSubmitForm}>
+            Add Item
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <h1>What wishlist is selected: {wishlistID}</h1>
     </>
   );
 };
