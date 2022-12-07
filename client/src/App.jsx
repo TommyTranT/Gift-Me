64d436f17eaa7f359e71772e6a47d713273e9833
Tommy Tran <haitran1995@gmail.com>
2022-12-07 02:25:55 -0500

    remove unused imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,9 +16,7 @@ import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -38,7 +36,7 @@ function App() {
   const theme = useTheme();
   const [open, setOpen] = useState(false); // state for opening side bar
 
-  const drawerWidth = 240;
+  const drawerWidth = 260;
 
   const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
     ({ theme, open }) => ({
