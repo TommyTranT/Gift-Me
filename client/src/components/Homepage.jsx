e76b3a25695909f6494ffa7712b0f5b0ce22b6b3
Tommy Tran <haitran1995@gmail.com>
2022-12-10 04:13:29 -0500

    add homepage components

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hero from "./Hero";
+import WishlistAd from "./WishlistAd";
+import ItemAd from "./ItemAd";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <section>
+      <Hero />
+      <WishlistAd />
+      <ItemAd />
+    </section>
+  );
 };
 
 export default Homepage;
---
+++ client/src/components/ItemAd.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ItemAd = () => {
+  return <div>ItemAd</div>;
+};
+
+export default ItemAd;
