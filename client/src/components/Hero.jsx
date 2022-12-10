c5911665c2a57f099efbb70c7754615939749bf0
Tommy Tran <haitran1995@gmail.com>
2022-12-10 04:53:52 -0500

    import assets

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { hero } from "../assets/";
+import styles from "../style";
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+const Hero = () => (
+  <section>
+    <div>
+      <div>
+        <h1> Make whatever list you want with the products you care about</h1>
+      </div>
+      <div>
+        <img src={hero} />
+      </div>
+    </div>
+  </section>
+);
 
 export default Hero;
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Hero from "./Hero";
 import WishlistAd from "./WishlistAd";
 import ItemAd from "./ItemAd";
+import styles from "../style";
 
 const Homepage = () => {
   return (
     <section>
-      <Hero />
+      <div>
+        <Hero />
+      </div>
+
       <WishlistAd />
       <ItemAd />
     </section>
