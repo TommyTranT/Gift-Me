514de6463939add01751638341d37552be1d679f
Tommy Tran <haitran1995@gmail.com>
2022-12-10 10:33:19 -0500

    styled component

--- client/src/components/WishlistAd.jsx
+++ client/src/components/WishlistAd.jsx
@@ -1,11 +1,16 @@
 import { wishlist } from "../assets";
 
 const WishlistAd = () => (
-  <section>
-    <div>
+  <section style={{ marginTop: "15%" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
       <img src={wishlist} alt="wishlistAd" />
-      <div>
-        <p>Make a list for</p>
+      <div style={{ fontSize: "60px", maxWidth: "500px" }}>
+        <h1>Make a list for anything you can think of.</h1>
       </div>
     </div>
   </section>
